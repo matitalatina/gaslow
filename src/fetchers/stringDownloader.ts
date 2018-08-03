7e8b17e14f3e23bc882112d17c6508499825e1b0
Mattia Natali <matitalatina@gmail.com>
2018-08-03 19:53:02 +0200

    Improve fetcher with both http and https

--- src/fetchers/stringDownloader.ts
+++ src/fetchers/stringDownloader.ts
@@ -1,9 +1,11 @@
 import http from "http";
+import https from "https";
 export class StringDownloader {
   download(url: string): Promise<string> {
+    const fetcher: any = url.toLowerCase().startsWith("https") ? https : http;
     return new Promise((resolve, reject) => {
       let data = "";
-      const request = http.get(url, function (res) {
+      const request = fetcher.get(url, function (res: any) {
 
         res.on("data", function (chunk: string) {
           data += chunk;
--- test/fetchers/stringDownloader.test.ts
+++ test/fetchers/stringDownloader.test.ts
@@ -4,8 +4,8 @@ import chai from "chai";
 const expect = chai.expect;
 
 describe("StringDownloader", () => {
-  it("should download the README.md as string", () => {
-    new StringDownloader()
+  it("should download the README.md as string (https)", () => {
+    return new StringDownloader()
       .download("https://raw.githubusercontent.com/matitalatina/gaslow/master/README.md")
       .then((file) => {
         expect(file).to.contains("GasLow");
