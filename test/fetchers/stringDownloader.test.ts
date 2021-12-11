ebe22ee406549f7b50b93aa20343b4eee92c34cf
Mattia Natali <matitalatina@gmail.com>
2021-12-11 19:24:06 +0100

    Upgraded axios

--- test/fetchers/stringDownloader.test.ts
+++ test/fetchers/stringDownloader.test.ts
@@ -1,9 +1,9 @@
 import { StringDownloader } from '../../src/fetchers/stringDownloader'
 
 describe('StringDownloader', () => {
-  it('should download the README.md as string (https)', () => StringDownloader
-    .download('https://raw.githubusercontent.com/matitalatina/gaslow/master/README.md')
-    .then((file) => {
-      expect(file).toContain('GasLow')
-    }))
+  it('should download the README.md as string (https)', async () => {
+    const file = await StringDownloader
+      .download('https://raw.githubusercontent.com/matitalatina/gaslow/master/README.md')
+    expect(file).toContain('GasLow')
+  })
 })
